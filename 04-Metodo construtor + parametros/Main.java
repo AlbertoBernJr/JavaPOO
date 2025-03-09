13f86477381e09dc54bba875edf308ce44328245
Alberto Bernardo Junior <alberto.bernardo.jr1@gmail.com>
2025-03-09 10:52:53 -0300

    Create Main.java

---
+++ 04-Metodo construtor + parametros/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Caneta c1=new Caneta("bic", "preto", 0.5f); 
+        //criando o objeto [caneta] usando o metodo construtor
+      
+        c1.status();
+    }
+}
