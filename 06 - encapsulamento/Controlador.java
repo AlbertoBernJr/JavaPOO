594d27f9a36829a2d94c149bbbd4210836e94bf6
Alberto Bernardo Junior <alberto.bernardo.jr1@gmail.com>
2025-03-10 23:39:58 -0300

    Create Controlador.java

---
+++ 06 - encapsulamento/Controlador.java
@@ -0,0 +1,12 @@
+public interface Controlador {
+    //somente a lista de métodos
+    //os métodos serão desenvolvidos na classe [ControleRemoto]
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void abrirmenu();
+    public abstract void fecharmenu();
+    public abstract void maisvolume();
+    public abstract void menosvolume();
+    public abstract void play();
+    public abstract void pause();
+}
