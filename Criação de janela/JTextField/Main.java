e5bf5db266c9423725c9dc910bf999465eaf945c
Alberto Bernardo Junior <alberto.bernardo.jr1@gmail.com>
2025-03-27 08:59:36 -0300

    Create Main.java

---
+++ Criação de janela/JTextField/Main.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Exemplo JTextField");
+        JTextField campoTexto = new JTextField(20); // 20 = largura aproximada em caracteres
+
+        frame.setLayout(new java.awt.FlowLayout());
+        frame.add(new JLabel("Digite seu nome:")); 
+        //criando um conteudo [JLabel] na janela [JFrame]
+      
+        frame.add(campoTexto); //adicionando o conteudo de [JTextField] à janela [JFrame]
+        frame.setSize(300, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
